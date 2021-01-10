703d4b1bd95934733017b9ea4c896c13b1c4e9b9
Katia <katrusia.tsaruk@gmail.com>
2021-01-10 19:23:52 +0200

    first commit

---
+++ dz/dz/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Security.Permissions;
+
+namespace dz
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    public class Block
+    {
+        
+    }
+
+    public class Sword : Block, IHit
+    {
+        public void Durability()
+        {
+
+        }
+
+        public void Damage()
+        {
+            
+        }
+    }
+
+    public class DiamondSword: Sword
+    {
+        const int DamagePerHit = 20;
+        SwordMaterials swordMaterial = SwordMaterials.Diamond;
+    }
+
+    public interface IHit
+    {
+        void Durability();
+        void Damage();
+    }
+    
+    public enum SwordMaterials
+    {
+        Wood,
+        Stone,
+        Iron,
+        Gold,
+        Diamond
+    }
+
+    public interface IMovable
+    {
+        bool TryToMove();
+        void Move();
+    }
+
+    public class Boat: Block, IMovable, IBreaking
+    {
+        public bool TryToMove()
+        {
+            return true;
+        }
+        public void Move()
+        {
+            
+        }
+
+        public void Health()
+        {
+            
+        }
+        public const int BoatSpeed=10;
+    }
+
+    public interface IBreaking
+    {
+        void Health();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
